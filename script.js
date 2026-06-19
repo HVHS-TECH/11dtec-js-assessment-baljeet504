1f72af229030d4fc34f9a0bba854769450f37e23
baljeet504 <26527ns@hvhs.school.nz>
2026-06-19 02:55:37 +0000

    Jun 19, 2026, 2:55 AM

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 /***Variables***/
 var price1 = 5
-var price2 = 10
-var price3 = 18
-var price4 = 3
+
 
 
 
@@ -10,4 +8,4 @@ var price4 = 3
 
 
 /***main code ***/
- <input type="text" id="nameField" name="formname" placeHolder="Name"
+ 
