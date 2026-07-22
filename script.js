03e9274426290621ce2becdf64218349f19e9864
baljeet504 <26527ns@hvhs.school.nz>
2026-07-22 00:03:48 +0000

    Jul 22, 2026, 12:03 AM

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ var userName;
 /***main code ***/
  
 alert("wellcome to Ryu's ramen");
-userName = prompt()
+userName = prompt("what is your name");
 alert("Hello" +userName+ "i will take youre order");
