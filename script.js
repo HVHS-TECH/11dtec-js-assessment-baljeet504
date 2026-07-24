c88f9c5b1d28b9389bf57176f6c7c15a0da80211
baljeet504 <26527ns@hvhs.school.nz>
2026-07-24 02:30:02 +0000

    Jul 24, 2026, 2:30 AM

--- script.js
+++ script.js
@@ -9,4 +9,4 @@ var food;
 alert("wellcome to Ryu's ramen");
 userName = prompt("what is your name");
 alert("Hello " +userName+ " i will take youre order");
- food = prompt("Would you like dimsum, ramen, scatdoughts")
+ food = prompt("we currently have dimsum, ramen, porkbelly and ")
