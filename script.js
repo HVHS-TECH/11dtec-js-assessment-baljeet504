2f2d7ec0fa3157e1f3baaefed38fa8745061f84b
baljeet504 <26527ns@hvhs.school.nz>
2026-07-24 02:53:36 +0000

    Jul 24, 2026, 2:53 AM

--- script.js
+++ script.js
@@ -11,4 +11,4 @@ userName = prompt("what is your name");
 alert("Hello " +userName+ " i will take youre order");
  food = prompt("we currently have dimsum, ramen, porkbelly and chicken feet");
  alert("so you would like " +food+ " is that correct?");
- 
+ alert("that will be 12bucks please pay at the counter");
